8b91da624c49cb4a06085b269523ce856e941479
manchandajayant <maanjayant@gmail.com>
2021-10-24 17:55:36 +0530

    pg version changed

--- db.js
+++ db.js
@@ -2,12 +2,7 @@ const Sequelize = require("sequelize");
 const databaseURL =
   process.env.DATABASE_URL ||
   "postgres://postgres:verber@127.0.0.1:5432/postgres";
-const db = new Sequelize(databaseURL, {
-  dialect: 'postgres',
-  dialectOptions: {
-    ssl: true
-  }, //solved the problem with self signed sertificate
-});
+const db = new Sequelize(databaseURL);
 
 db.sync()
   .then(() => console.log("Database is in  Sync"))
