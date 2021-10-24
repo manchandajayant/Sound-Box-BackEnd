850c0adc69d3afeacb4b5f366b51b1f8a64fc1b1
manchandajayant <maanjayant@gmail.com>
2021-10-24 16:08:22 +0530

    copy lines removed

--- db.js
+++ db.js
@@ -2,28 +2,9 @@ const Sequelize = require("sequelize");
 const databaseURL =
   process.env.DATABASE_URL ||
   "postgres://postgres:verber@127.0.0.1:5432/postgres";
-const db = new Sequelize({
-  connectionString: databaseURL,
-  ssl: {
-    rejectUnauthorized: false,
-  },
-});
-
-db.sync()
-  .then(() => console.log("Database is in  Sync"))
-  .catch(console.error);
-
-module.exports = db;
-const Sequelize = require("sequelize");
-const databaseURL =
-  process.env.DATABASE_URL ||
-  "postgres://postgres:verber@127.0.0.1:5432/postgres";
-const db = new Sequelize({
-  connectionString: databaseURL,
-  ssl: {
-    rejectUnauthorized: false,
-  },
-});
+const db = new Sequelize({connectionString:databaseURL,  ssl: {
+  rejectUnauthorized: false
+}});
 
 db.sync()
   .then(() => console.log("Database is in  Sync"))
