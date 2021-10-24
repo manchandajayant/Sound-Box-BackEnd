134b77387a233e9cdc941026292455daaf7bcbe0
manchandajayant <maanjayant@gmail.com>
2021-10-24 16:06:05 +0530

    db connection ssl enabled

--- db.js
+++ db.js
@@ -1,8 +1,29 @@
 const Sequelize = require("sequelize");
 const databaseURL =
   process.env.DATABASE_URL ||
-  "postgres://postgres:sound@localhost:5432/postgres";
-const db = new Sequelize(databaseURL);
+  "postgres://postgres:verber@127.0.0.1:5432/postgres";
+const db = new Sequelize({
+  connectionString: databaseURL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
+});
+
+db.sync()
+  .then(() => console.log("Database is in  Sync"))
+  .catch(console.error);
+
+module.exports = db;
+const Sequelize = require("sequelize");
+const databaseURL =
+  process.env.DATABASE_URL ||
+  "postgres://postgres:verber@127.0.0.1:5432/postgres";
+const db = new Sequelize({
+  connectionString: databaseURL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
+});
 
 db.sync()
   .then(() => console.log("Database is in  Sync"))
